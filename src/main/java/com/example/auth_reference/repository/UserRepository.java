257842724092e02deed7dc4cb100d4641e1efcfa
JeffTunner <abhinavkaushik05@gmail.com>
2026-02-02 19:54:10 +0530

    created user repository to store user info in db

---
+++ src/main/java/com/example/auth_reference/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.auth_reference.repository;
+
+import com.example.auth_reference.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+
+    Optional<UserInfo> findByUsername(String username);
+}
