93d3afd4c8c1cc7cb5903ae3cb422efca6fc84d7
JeffTunner <abhinavkaushik05@gmail.com>
2026-02-03 16:13:43 +0530

    Updated the login method to actually authenticate user using the stored data and jwt

--- src/main/java/com/example/auth_reference/service/UserService.java
+++ src/main/java/com/example/auth_reference/service/UserService.java
@@ -2,9 +2,14 @@ package com.example.auth_reference.service;
 
 import com.example.auth_reference.entity.UserInfo;
 import com.example.auth_reference.repository.UserRepository;
+import com.example.auth_reference.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,11 +20,28 @@ public class UserService {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    JwtService jwtService;
+
     public UserInfo register(String username, String password) {
         String hashedPassword = passwordEncoder.encode(password);
         UserInfo user = new UserInfo();
         user.setUsername(username);
         user.setPassword(hashedPassword);
         return userRepository.save(user);
+    }
+
+    public Map<String, String> login(String username, String password) {
+        UserInfo user = userRepository.findByUsername(username);
+        if(user == null) {
+            throw new NullPointerException("User not found!");
+        }
+        if(passwordEncoder.matches(password, user.getPassword())) {
+            List<String> roles = List.of("ROLE_USER");
+            String token = jwtService.generateToken(username, roles);
+            return Map.of("token", token);
+        } else {
+            throw new IllegalStateException("Password doesnt match");
+        }
     }
 }
