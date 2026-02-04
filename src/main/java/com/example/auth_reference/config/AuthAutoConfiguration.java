f5fb05cb4f335d70e61a01564141ecbe5c5405f1
JeffTunner <abhinavkaushik05@gmail.com>
2026-02-04 20:29:13 +0530

    Reusable Auth Module Completed with auto configuration

---
+++ src/main/java/com/example/auth_reference/config/AuthAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.auth_reference.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "com.example.auth_reference")
+public class AuthAutoConfiguration {
+}
