f3f19d827b3368c202712b59dfb0b4956eb24253
JeffTunner <abhinavkaushik05@gmail.com>
2026-02-14 00:20:26 +0530

    Integrated reusable auth successfully into a consumer app

--- src/main/java/com/example/auth_reference/security/SecurityConfig.java
+++ src/main/java/com/example/auth_reference/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.auth_reference.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/auth/**")
